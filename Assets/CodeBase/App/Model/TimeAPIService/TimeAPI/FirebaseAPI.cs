70ff54a216203d6489d5fe548c6b14a55b6fc857
Part-time knight <Sunoco.seal@gmail.com>
2024-09-08 14:17:39 +0300

    model for firebase

---
+++ Assets/CodeBase/App/Model/TimeAPIService/TimeAPI/FirebaseAPI.cs
@@ -0,0 +1,17 @@
+using Cysharp.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace App.Model.TimeAPIService.TimeAPI
+{
+    /*public class FirebaseAPI : AbstractTimeAPI
+    {
+        protected override UniTask GetTimeStamp()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        
+    }*/
+}
